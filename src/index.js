d976d8ce4253f313e581f84ba559a5fef08e0e96
Yash121l <yashlunawat121@gmail.com>
2024-10-16 15:27:17 +0530

    coded

--- src/index.js
+++ src/index.js
@@ -31,7 +31,60 @@ app.post("/create", async (req, res) => {
   }
 });
 
-//write your code here
+app.get("/getAll", async (req, res) => {
+  try {
+    const todos = await prisma.todos.findMany()
+    return res.status(200).json({ "todos": todos })
+  } catch (err) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+})
+
+app.patch("/update/:id", async (req, res) => {
+  const id = req.params.id
+  const { task, completed } = req.body
+  if (!task && !completed) res.status(404).json({ "message": "No fields provided to update" })
+  try {
+    const todo = await prisma.todos.findUnique({
+      where: { id: parseInt(id) }
+    })
+    if (!todo) res.status(404).json({ "message": "Todo not found" })
+
+      const todos = await prisma.todos.update({
+        where: {
+          id: parseInt(id)
+        },
+        data: 
+          req.body
+        
+      })
+      return res.status(200).json({ "message": "Todo is updated", "todos": todos })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send("Internal Server Erroe")
+  }
+
+})
+
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id
+  try {
+    const todo = await prisma.todos.findUnique({
+      where: { id: parseInt(id) }
+    })
+    if (!todo) res.status(404).json({ "message": "Todo not found" })
+    if (todo) {
+      await prisma.todos.delete({
+        where: { id: parseInt(id) }
+      })
+      res.status(200).json({ "message": "Todo is Deleted" })
+    }
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send("Internal Server Erroe")
+  }
+
+})
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
